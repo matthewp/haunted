6b0fe2918ea669163a46c97a518b8f1c79b878b1
Matthew Phillips <phillipsm2@gmail.com>
2018-11-02 17:57:28 -0400

    Adds a test to verify that properties are passed
    The first time a component renders.

---
+++ test/test-props.js
@@ -0,0 +1,31 @@
+import { component, html } from '../web.js';
+import { attach, later } from './helpers.js';
+
+describe('Passing props', () => {
+  it('Are passed in the initial render', async () => {
+    const tag = 'props-first-run-test';
+    let runs = 0;
+
+    function child({ prop }) {
+      assert.equal(prop.id, 1);
+      runs++;
+      return html`testing ${prop.id}`;
+    }
+
+    customElements.define(tag + '-child', component(child));
+
+    function app() {
+      return html`
+        <props-first-run-test-child .prop=${{id:1}}>
+        </props-first-run-test-child>
+      `;
+    }
+    customElements.define(tag, component(app));
+
+    const teardown = attach(tag);
+    await later();
+    
+    assert.equal(runs, 1, 'child only ran once');
+    teardown();
+  });
+});
