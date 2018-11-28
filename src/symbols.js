81941a933a21bdef472a6ee4fb289d563c131cdf
Cris Ward <cris@duodesign.co.uk>
2018-11-28 13:08:24 +0000

    super simple fallback for IE11

--- src/symbols.js
+++ src/symbols.js
@@ -1,9 +1,11 @@
-export const phaseSymbol = 'haunted.phase'
-export const hookSymbol = 'haunted.hook'
+const symbolFor = typeof Symbol === 'function' ? Symbol.for : str => str;
 
-export const updateSymbol = 'haunted.update'
-export const commitSymbol = 'haunted.commit'
-export const effectsSymbol = 'haunted.effects'
-export const contextSymbol = haunted.context'
+export const phaseSymbol = symbolFor('haunted.phase');
+export const hookSymbol = symbolFor('haunted.hook');
+
+export const updateSymbol = symbolFor('haunted.update');
+export const commitSymbol = symbolFor('haunted.commit');
+export const effectsSymbol = symbolFor('haunted.effects');
+export const contextSymbol = symbolFor('haunted.context');
 
 export const contextEvent = 'haunted.context'; 
