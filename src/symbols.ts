140dd6fcc4146a08e6b6b42a2e1ddc3b8a987bcd
Enzo Baldisserri <enzo.baldisserri@outlook.fr>
2019-09-13 22:06:17 +0200

    refactor(symbols): use private symbols

--- src/symbols.ts
+++ src/symbols.ts
@@ -1,10 +1,10 @@
-const phaseSymbol = Symbol.for('haunted.phase');
-const hookSymbol = Symbol.for('haunted.hook');
+const phaseSymbol = Symbol('haunted.phase');
+const hookSymbol = Symbol('haunted.hook');
 
-const updateSymbol = Symbol.for('haunted.update');
-const commitSymbol = Symbol.for('haunted.commit');
-const effectsSymbol = Symbol.for('haunted.effects');
-const layoutEffectsSymbol = Symbol.for('haunted.layoutEffects');
+const updateSymbol = Symbol('haunted.update');
+const commitSymbol = Symbol('haunted.commit');
+const effectsSymbol = Symbol('haunted.effects');
+const layoutEffectsSymbol = Symbol('haunted.layoutEffects');
 
 type EffectsSymbols = typeof effectsSymbol | typeof layoutEffectsSymbol;
 type Phase = typeof updateSymbol | typeof commitSymbol | typeof effectsSymbol;
