a48d2f2eec0127133e70a36e5aecc89a31580db3
Mika <mika@wappia.se>
2020-06-18 19:12:23 +0800

    Don't override native properties

--- src/component.ts
+++ src/component.ts
@@ -113,6 +113,7 @@ function makeComponent(render: RenderFunction): Creator {
           }
 
           Reflect.set(target, key, value, receiver);
+          return true
         }
 
         if(typeof key === 'symbol' || key[0] === '_') {
