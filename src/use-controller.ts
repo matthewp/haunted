9d0d5f35f4c3c2cff0e61dd2e2e2aa9266c63be1
Benny Powers <web@bennypowers.com>
2021-05-04 22:02:47 +0300

    fix: replace google BSD-3-Clause license in useController

--- src/use-controller.ts
+++ src/use-controller.ts
@@ -1,3 +1,9 @@
+/**
+ * @license
+ * Portions Copyright 2021 Google LLC
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
 import { ReactiveController, ReactiveControllerHost } from '@lit/reactive-element';
 
 import { useLayoutEffect } from './use-layout-effect';
