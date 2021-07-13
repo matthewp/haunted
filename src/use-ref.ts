85b58096d59570b5366ea70cdf2d6c706ef0ec8e
Benny Powers <web@bennypowers.com>
2021-07-13 21:45:42 +0300

    docs: useRef

--- src/use-ref.ts
+++ src/use-ref.ts
@@ -1,5 +1,11 @@
 import { useMemo } from './use-memo';
 
+/**
+ * @function
+ * @template T
+ * @param   {T} initialValue
+ * @return  {{ current: T }} Ref
+ */
 const useRef = <T>(initialValue: T) => useMemo(() => ({
     current: initialValue
 }), []);
