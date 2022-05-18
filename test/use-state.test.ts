9c9bd24daed640e9e02e2174518b7b8ba72c70f2
Matthew Phillips <matthew@matthewphillips.info>
2022-05-18 14:38:31 -0400

    useState: Prevent updating when the value hasn't changed  (#392)
    * fix: useState updater
    
    * Prevent updating when the value hasn't changed
    
    Closes #273
    
    * Use a changeset
    
    * Use Object.is for comparison
    
    Co-authored-by: Mateusz Olsztyński <38963051+mattheol@users.noreply.github.com>

--- test/use-state.test.ts
+++ test/use-state.test.ts
@@ -24,4 +24,29 @@ describe('useState', () => {
     await nextFrame();
     expect(span.textContent).to.equal('33');
   });
+
+  it('Updater function should only trigger rerender if state has changed', async () => {
+    const tag = 'use-state-callback-two';
+    let setter, runs = 0;
+
+    function App() {
+      runs++;
+      let [age, setAge] = useState(() => 8);
+      setter = setAge;
+      return html`<span>${age}</span>`;
+    }
+
+    customElements.define(tag, component(App));
+
+    const el = await fixture<HTMLElement>(html`<use-state-callback-two></use-state-callback-two>`);
+
+    let span = el.shadowRoot.firstElementChild;
+    expect(span.textContent).to.equal('8');
+
+    setter(8);
+
+    await nextFrame();
+
+    expect(runs).to.equal(1);
+  });
 });
