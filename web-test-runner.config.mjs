18127bbcd2cab2f0e0392685ceb27a424360abd8
Meghea Iulian <iulian.meghea@gmail.com>
2025-02-18 09:44:09 +0200

    Add support for Lit 3
    Update more dependencies and support Lit 3.

--- web-test-runner.config.mjs
+++ web-test-runner.config.mjs
@@ -1,5 +1,6 @@
-import { esbuildPlugin } from '@web/dev-server-esbuild';
+import { esbuildPlugin } from "@web/dev-server-esbuild";
 
 export default {
+  nodeResolve: true,
   plugins: [esbuildPlugin({ ts: true })],
 };
