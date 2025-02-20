b32d14d4cb9b76be9d11e37322b2dfd081035ebf
Meghea Iulian <iulian.meghea@gmail.com>
2025-02-20 15:27:26 +0200

    Adjust web-test-runner config

--- web-test-runner.config.mjs
+++ web-test-runner.config.mjs
@@ -3,4 +3,10 @@ import { esbuildPlugin } from "@web/dev-server-esbuild";
 export default {
   nodeResolve: true,
   plugins: [esbuildPlugin({ ts: true })],
+  files: ["**!(node_modules)/*.test.(j|t)s"],
+  testFramework: {
+    config: {
+      timeout: "5000",
+    },
+  },
 };
